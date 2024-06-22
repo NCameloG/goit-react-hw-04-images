a9d4edd3aafc6c4e4ca8157a39fe5b5a2cbd410f
NCameloG <dev.ncamelog@gmail.com>
2024-06-22 16:50:04 -0500

    Modal fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const ImageGalleryItem = ({ image, openModal }) => {
-  const handleImageClick = () => {
-    openModal(image.id);
-  };
-
+const Modal = ({ imageUrl, onClose }) => {
   return (
-    <li className="gallery-item" onClick={handleImageClick} key={image.id}>
-      <img src={image.webformatURL} alt="" />
-    </li>
+    <div className="overlay" onClick={onClose}>
+      <div className="modal" onClick={e => e.stopPropagation()}>
+        <img src={imageUrl} alt="" />
+      </div>
+    </div>
   );
 };
 
-export default ImageGalleryItem;
+export default Modal;
